958d4daf4cbf6bc7c406f9db790062bfe6c28bf0
Laércio de Sousa <laerciosousa@sme-mogidascruzes.sp.gov.br>
2018-06-12 16:30:35 -0300

    Fix credits in file udev-devices.c.

--- src/multi-seat-config/udev-devices.c
+++ src/multi-seat-config/udev-devices.c
@@ -1,3 +1,43 @@
+/*
+ * Copyright (C) 2004-2007 Centro de Computacao Cientifica e Software Livre
+ * Departamento de Informatica - Universidade Federal do Parana - C3SL/UFPR
+ *
+ * Copyright (C) 2018 Prefeitura de Mogi das Cruzes, SP
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+ * USA.
+ *
+ *
+ * This program monitors evdev device nodes passed as parameters to detect the
+ * F1...F12, ENTER or ESC keys pressed or the left mouse button.
+ *
+ * The first argument is the key that we need to check.
+ *   - If between f1 and f12, the argument must be between 1 and 12
+ *   - If it's the left mouse button, the argument is 13
+ *   - If it's enter or esc, the argument is 14
+ * The other arguments are the device node file paths
+ *
+ * The timeout is 20 seconds.
+ * If "enter" or "esc" is requested, the timeout is 5 seconds.
+ *
+ * The output is:
+ *   - "device/node/path" for f1..f12 and mouse button
+ *   - "enter" or "esc" for enter and esc keys (no device path)
+ *   - "timeout" if no key is pressed after timeout has reached
+ */
+
 #include <fcntl.h>
 #include <libudev.h>
 #include <stdbool.h>
